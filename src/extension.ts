2f5cba950a2993c68f77cdf6b6b52d812dc9bc02
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2026-03-13 18:49:58 +0500

    implement tree view provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,24 @@
 import { LanguageClient } from "vscode-languageclient/node";
 import * as vscode from "vscode";
 import { createLanguageClient } from "./client/client";
+import { TreeViewProvider } from "./views/treeViewProvider";
 
 let client: LanguageClient;
+let treeView: TreeViewProvider;
 
 export function activate(context: vscode.ExtensionContext) {
   client = createLanguageClient(context);
   client.start();
+
+  treeView = new TreeViewProvider(client, context);
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("ibralogue.openTreeView", (uri?: vscode.Uri) => {
+      treeView.show(uri);
+    }),
+  );
+
+  context.subscriptions.push(treeView);
 }
 
 export function deactivate(): Thenable<void> | undefined {
